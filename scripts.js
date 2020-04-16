aef8a87e1fd0588576fc61b67a70bf56bc131cd4
barathdani <47307421+barathdani@users.noreply.github.com>
2020-04-16 08:23:07 +0200

    first_3_parts

---
+++ scripts.js
@@ -0,0 +1,53 @@
+console.log("javascript is working");
+
+let post = {
+    title: "My post",
+    description: "My first post, awesome",
+    likeCount: 12,
+    comments: ["cool!", "nice job!"]
+};
+
+let colors = ["orange", "blue", "green", "purple"];
+
+console.log(post);
+console.log(colors);
+
+
+let age = 33;
+if (age > 18){
+  console.log("You are an adult");
+}
+else {
+  console.log("you're a kid!");
+}
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+ let posts = [{
+   title: "First post",
+   likeCount: 12
+ }, {
+   title: "Second post",
+   likeCount: 222
+ }];
+
+ console.log(colors);
+
+ let myFunction = x => {
+   console.log(x);
+ }
+
+ let greeter = name => {
+   console.log(`Hello, ${name}`);
+ }
+
+ greeter("Adam");
+ greeter("Peter");
+ greeter("Máté");
+
+colors.forEach(color =>{
+  console.log(color);
+})
+
+
+console.log("this is the end of the code");
